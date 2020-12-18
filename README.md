b674708eb2c000dbd91675551483e20fe6d972bd
Gerald Schnabel <gschnabel@gmx.de>
2020-12-18 13:48:51 +0100

    script with binutils-2.35.1, gcc-10.2.0, newlib-4.0.0

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# install_arm-none-eabi-gcc.sh
-A bash script to create a cross-compilation environment for the arm-none-eabi target
+install_arm-none-eabi-gcc.sh
+============================
+
+This is an updated version of the `install_arm-none-eabi-gcc.sh` bash script from the
+[MiST devel repository](https://github.com/mist-devel/mist-board/blob/master/tools/install_arm-none-eabi-gcc.sh)
+to create a cross-compilation environment for the arm-none-eabi target based on the blog post 
+[Trials, Tribulations and Toolchains](http://retroramblings.net/?p=315).
+
+The resulting gcc compiler is used to compile the [MiST firmware](https://github.com/mist-devel/mist-firmware).
+The [Canon Hack Development Kit](https://chdk.fandom.com/wiki/CHDK) (CHDK) can be compiled with it, too.
+
+Currently used components:
+* [binutils 2.35.1](https://ftp.gnu.org/gnu/binutils/)
+* [gcc 10.2.0](https://ftp.gnu.org/gnu/gcc/)
+* [newlib 4.0.0](https://sourceware.org/pub/newlib/)
---
+++ install_arm-none-eabi-gcc.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+# Script to install gcc as described on 
+# http://retroramblings.net/?p=315
+
+PREFIX='/opt/mist/arm-none-eabi'
+
+ARCHIVES_DIR='archives'
+
+BINUTILS_URL='ftp://ftp.fu-berlin.de/unix/gnu/binutils/'
+BINUTILS_VERSION='binutils-2.35.1'
+BINUTILS_ARCHIVE="${BINUTILS_VERSION}.tar.xz"
+BINUTILS_MD5='7126f370ffbd46c08fcc5ce7aee2805d'
+
+GCC_URL='ftp://ftp.fu-berlin.de/unix/languages/gcc/releases/'
+GCC_VERSION='gcc-10.2.0'
+GCC_ARCHIVE="${GCC_VERSION}.tar.xz"
+GCC_MD5='e9fd9b1789155ad09bcf3ae747596b50'
+
+NEWLIB_URL='ftp://sourceware.org/pub/newlib/'
+NEWLIB_VERSION='newlib-4.0.0'
+NEWLIB_ARCHIVE="${NEWLIB_VERSION}.tar.gz"
+NEWLIB_MD5='22d11117d94fe5f2dade56721c62629c'
+
+CPUS=$(nproc)
+JOBS=1
+if [[ ${CPUS} -gt 1 ]]; then
+  JOBS=$((${CPUS} - 1))
+fi
+
+if [[ ! -d "${ARCHIVES_DIR}" ]]; then
+    mkdir "${ARCHIVES_DIR}"
+fi
+
+if [[ ! -f "${ARCHIVES_DIR}"/"${BINUTILS_ARCHIVE}" ]]; then
+    echo "Downloading ${BINUTILS_ARCHIVE} ..."
+    wget -P "${ARCHIVES_DIR}" "${BINUTILS_URL}${BINUTILS_ARCHIVE}"
+fi
+
+if [[ $(md5sum -b "${ARCHIVES_DIR}"/"${BINUTILS_ARCHIVE}" | cut -d' ' -f1) != "${BINUTILS_MD5}" ]]; then
+    echo "Archive is broken: ${BINUTILS_ARCHIVE}"
+    exit 1
+fi
+
+if [[ ! -f "${ARCHIVES_DIR}"/"${GCC_ARCHIVE}" ]]; then
+    echo "Downloading ${GCC_ARCHIVE} ..."
+    wget -P "${ARCHIVES_DIR}" "${GCC_URL}${GCC_VERSION}"/"${GCC_ARCHIVE}"
+fi
+
+if [[ $(md5sum -b "${ARCHIVES_DIR}"/"${GCC_ARCHIVE}" | cut -d' ' -f1) != "${GCC_MD5}" ]]; then
+    echo "Archive is broken: ${GCC_ARCHIVE}"
+    exit 1
+fi
+
+if [[ ! -f "${ARCHIVES_DIR}"/"${NEWLIB_ARCHIVE}" ]]; then
+    echo "Downloading ${NEWLIB_ARCHIVE} ..."
+    wget -P "${ARCHIVES_DIR}" "${NEWLIB_URL}${NEWLIB_ARCHIVE}"
+fi
+
+if [[ $(md5sum -b "${ARCHIVES_DIR}"/"${NEWLIB_ARCHIVE}" | cut -d' ' -f1) != "${NEWLIB_MD5}" ]]; then
+    echo "Archive is broken: ${NEWLIB_ARCHIVE}"
+    exit 1
+fi
+
+# ------------------------ build binutils ------------------
+echo "Building ${BINUTILS_VERSION}, jobs: ${JOBS}"
+
+if [[ -d "${BINUTILS_VERSION}" ]]; then
+    echo "Cleaning up previous build ..."
+    rm -rf "${BINUTILS_VERSION}" 
+fi
+
+tar xf "${ARCHIVES_DIR}"/"${BINUTILS_ARCHIVE}"
+cd "${BINUTILS_VERSION}"
+mkdir arm-none-eabi
+cd arm-none-eabi
+../configure --target=arm-none-eabi --prefix="${PREFIX}"
+make -j ${JOBS}
+make install
+cd ../../
+
+# ------------------------ build gcc ------------------
+export PATH="${PREFIX}/bin:${PATH}"
+
+echo "Building ${GCC_VERSION}, jobs: ${JOBS}"
+
+if [[ -d "${GCC_VERSION}" ]]; then
+    echo "Cleaning up previous build ..."
+    rm -rf "${GCC_VERSION}"
+fi
+
+tar xf "${ARCHIVES_DIR}"/"${GCC_ARCHIVE}"
+
+if [[ -d "${NEWLIB_VERSION}" ]]; then
+    echo "Cleaning up previous build ..."
+    rm -rf "${NEWLIB_VERSION}"
+fi
+
+tar xf "${ARCHIVES_DIR}"/"${NEWLIB_ARCHIVE}"
+
+cd "${GCC_VERSION}"
+ln -s ../"${NEWLIB_VERSION}"/newlib .
+mkdir arm-none-eabi
+cd arm-none-eabi
+../configure --target=arm-none-eabi --prefix="${PREFIX}" --enable-languages=c --with-newlib
+make -j ${JOBS}
+make install
+cd ../../
