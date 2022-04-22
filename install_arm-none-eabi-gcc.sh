a36ea2c0fb2f250a43a7a1c54ac2578f604feca1
Gerald Schnabel <gschnabel@gmx.de>
2022-04-22 08:04:33 +0200

    Update to gcc-11.3.0

--- install_arm-none-eabi-gcc.sh
+++ install_arm-none-eabi-gcc.sh
@@ -15,9 +15,9 @@ BINUTILS_ARCHIVE="${BINUTILS_VERSION}.tar.xz"
 BINUTILS_MD5='6e39cad1bb414add02b5b1169c18fdc5'
 
 GCC_URL='ftp://ftp.fu-berlin.de/unix/languages/gcc/releases/'
-GCC_VERSION='gcc-11.2.0'
+GCC_VERSION='gcc-11.3.0'
 GCC_ARCHIVE="${GCC_VERSION}.tar.xz"
-GCC_MD5='31c86f2ced76acac66992eeedce2fce2'
+GCC_MD5='4ee3e8c4c99e7b3444eb79f00f5f7a7e'
 
 NEWLIB_URL='ftp://sourceware.org/pub/newlib/'
 NEWLIB_VERSION='newlib-4.2.0.20211231'
