eaede5147fb73b8af7f09da0881c8e170fe5a488
Gerald Schnabel <gschnabel@gmx.de>
2022-08-06 00:08:10 +0200

    Update to binutils-2.39

--- install_arm-none-eabi-gcc.sh
+++ install_arm-none-eabi-gcc.sh
@@ -10,9 +10,9 @@ PREFIX="/opt/mist/${TARGET}"
 ARCHIVES_DIR='archives'
 
 BINUTILS_URL='ftp://ftp.fu-berlin.de/unix/gnu/binutils/'
-BINUTILS_VERSION='binutils-2.38'
+BINUTILS_VERSION='binutils-2.39'
 BINUTILS_ARCHIVE="${BINUTILS_VERSION}.tar.xz"
-BINUTILS_MD5='6e39cad1bb414add02b5b1169c18fdc5'
+BINUTILS_MD5='f7e986ae9ff06405cafb2e585ee36d27'
 
 GCC_URL='ftp://ftp.fu-berlin.de/unix/languages/gcc/releases/'
 GCC_VERSION='gcc-12.1.0'
